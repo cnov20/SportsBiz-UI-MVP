66ea337d706f7b4fede65bb98e80837fa4d0d1cb
Chris <Chris@Chriss-MacBook-Air.local>
2018-06-04 12:55:16 -0400

    Initial application creation and initial routing file created

---
+++ financial-dashboard-UI/src/app/app.routing.ts
@@ -0,0 +1,12 @@
+// Provides router to rest of application
+// Enables feature to render specific components based on URL specified
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Master list of all available routes
+// Declared as "const" to mitigate risk of
+// other portions of applcation, altering the array(master list)
+const appRoutes: Routes = [ ];
+
+// Makes list of routes available to rest of application, via the root module
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
