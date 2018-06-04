426ce63e0d3154222489e753892cc25b9824236d
Chris <Chris@Chriss-MacBook-Air.local>
2018-06-04 13:17:01 -0400

    Added new route object to routesmaster list, for welcome page component to be displayed -- with specified path and component prperties

--- financial-dashboard-UI/src/app/app.routing.ts
+++ financial-dashboard-UI/src/app/app.routing.ts
@@ -2,11 +2,19 @@
 // Enables feature to render specific components based on URL specified
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 // Master list of all available routes
 // Declared as "const" to mitigate risk of
 // other portions of applcation, altering the array(master list)
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [ 
+	
+	{
+		path: '',
+		component: WelcomeComponent
+	}
+
+];
 
 // Makes list of routes available to rest of application, via the root module
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
