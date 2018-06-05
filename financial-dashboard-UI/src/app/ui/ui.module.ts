e1dd07130ac87dd20302b77adc0ef9374ab1c451
Chris <Chris@Chriss-MacBook-Air.local>
2018-06-05 11:24:18 -0400

    Exported routing object, now available in ui module -- as such router links now working properly for navigation between pages. Also, as a result further modularization of css and styling. All styling preserved but modularized

--- financial-dashboard-UI/src/app/ui/ui.module.ts
+++ financial-dashboard-UI/src/app/ui/ui.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from '../app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { routing } from '../app.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    routing
   ],
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent]
 })
-export class UiModule { }
+export class UiModule {}
